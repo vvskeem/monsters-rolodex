541e3906fc6158868ec8c87cb9c7ff9df9026a0e
Shakeem <albouyshakeem@gmail.com>
2020-04-08 16:56:12 -0300

    Updates

---
+++ precache-manifest.7b864c0880cb6dda54ac5a3d4d47998c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a57a78ff548fd3a4fe81a6ba25d308",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4ce9db09fa5b0afd77b",
+    "url": "/monsters-rolodex/static/css/main.9e799a88.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ce9db09fa5b0afd77b",
+    "url": "/monsters-rolodex/static/js/main.bfd56131.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
